30f772fe29d1459c0511925202d2aee19e7a6a74
Guilherme Casali <guilhermecasali.dev@gmail.com>
2022-01-02 20:19:23 -0300

    created base game visual

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,37 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
 
-function App() {
+import "./App.scss";
+
+export function App() {
+  const [lastPressedKey, setLastPressedKey] = useState("");
+  const [isKeyGrowing, setIsKeyGrowing] = useState(false);
+
+  useEffect(() => {
+    document.addEventListener("keydown", (keyPressed: KeyboardEvent) => {
+      if (keyPressed.key.match(/^.$/)) {
+        setLastPressedKey(keyPressed.key);
+        setIsKeyGrowing(true);
+        setTimeout(() => {
+          setIsKeyGrowing(false);
+        }, 100);
+      }
+    });
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="comingSoon">
+        <h1>
+          <span className="timeout">setTimeout</span>
+          <span className="bracket">(</span>
+          <span className="variable">soon</span>
+          <span className="bracket">)</span>;
+        </h1>
+      </div>
+
+      <h1 className={"keyPressed" + (isKeyGrowing ? " grow" : "")}>
+        {lastPressedKey}
+      </h1>
     </div>
   );
 }
-
-export default App;
